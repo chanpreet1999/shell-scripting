f5594839e76a5d7d9c567b268d3caada0c7e51f4
Chanpreet Singh <34856246+chanpreet1999@users.noreply.github.com>
2021-05-15 17:05:24 +0530

    Create basics.sh

---
+++ basics.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+echo "hello from file" #this is a comment
+
+#these are system variables
+
+echo Our bash name is $BASH 	
+echo Our bash version is $BASH_VERSION
+echo Our pwd is $PWD
+echo Our home directory is $HOME
+
+name=chanpreet
+
+echo Our name is $name
+
